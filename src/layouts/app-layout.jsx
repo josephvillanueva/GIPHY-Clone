a6a4bf4b0c782d7fe23270628ec7909cf2714adb
Joseph Villanueva <josephgvillanueva24@gmail.com>
2024-05-07 01:55:14 +0800

    setting up navigation

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div>
-      {/* header */}
-      <main>
-        <Outlet />
-      </main>
+    <div className="bg-gray-500 text-white min-h-screen">
+      <div className="container px-6 py-4 mx-auto">
+        {/* header */}
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
